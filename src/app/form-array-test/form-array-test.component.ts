0e625d2b80b5646ba678aa8ab6a0034e71391522
Pablo Ferreira <pablo_ferreira14@hotmail.com>
2022-11-18 06:14:17 -0300

    mais e mais tentativas

--- src/app/form-array-test/form-array-test.component.ts
+++ src/app/form-array-test/form-array-test.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Store } from '../interfaces/store';
 
 @Component({
@@ -7,12 +7,11 @@ import { Store } from '../interfaces/store';
   templateUrl: './form-array-test.component.html',
   styleUrls: ['./form-array-test.component.scss'],
 })
-export class FormArrayTestComponent implements OnInit {
-  form!: FormGroup;
+export class FormArrayTestComponent {
+  form: FormGroup;
 
   panelOpenState = false;
-  dataStores: Store[] = [];
-  storeId!: string;
+  storeId!: '123123xxx-123123-xxxx';
 
   listModality: any[] = [
     { id: '3fa85f64-5717-4562-b3fc-2c963f66afAA', name: 'Modalidade 1' },
@@ -23,221 +22,140 @@ export class FormArrayTestComponent implements OnInit {
   listSalesChannel: any[] = [
     {
       salesChannelId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-      salesChannelName: 'lista de canais 1',
+      salesChannelName: 'canal 1',
       modalityId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
       modalityName: 'modalidade 1',
     },
     {
       salesChannelId: '3fa85f64-5717-4562-b3fc-2c963f66af33',
-      salesChannelName: 'lista de canais 2',
+      salesChannelName: 'canal 2',
       modalityId: '3fa85f64-5717-4562-b3fc-2c963f66af33',
       modalityName: 'modalidade 2',
     },
   ];
 
-  constructor(private fb: FormBuilder) {}
+  dataStores: Store[] = [
+    {
+      storeId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+      storeName: 'NOME',
+      storeType: {
+        id: 0,
+        name: 'string',
+        nickName: 'string',
+        active: true,
+      },
+      brand: {
+        id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+        name: 'string',
+        image: 'string',
+        active: true,
+      },
+      storeAddress: {
+        id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+        storeId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+        street: 'string',
+        number: 'string',
+        zipCode: 'string',
+        additional: 'string',
+        neighborhood: 'string',
+        city: 'string',
+        state: 'string',
+        country: 'string',
+        latitude: 0,
+        longitude: 0,
+        urlMaps: 'string',
+      },
+      contacts: [
+        {
+          id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+          name: 'string',
+          email: 'string',
+          phone: 0,
+          contactTypeId: 0,
+          contactTypeName: 'string',
+        },
+      ],
+      storeModalitys: [
+        {
+          storeModalityId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+          storeCode: 'code store 1',
+          cashierCode: 'code store 1',
+          modality: {
+            id: '3fa85f64-5717-4562-b3fc-2c963f66afAA',
+            name: 'string',
+            tag: 'string',
+            active: true,
+            order: 0,
+            canBlock: true,
+          },
+          openingHours: [
+            {
+              id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+              storeModalityId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+              dayWeek: 0,
+              initialTime: 'string',
+              endTime: 'string',
+            },
+          ],
+          salesChannel: [
+            {
+              storeModalitySalesChannelId:
+                '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+              salesChannelId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+              salesChannelName: 'string',
+            },
+          ],
+        },
+      ],
+    },
+  ];
 
-  get stores() {
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      stores: this.formBuilder.array([])
+    });
+
+    // this.dataStores.map((item) => item.storeModalitys.forEach(() => {
+    //   this.addStoreModalitys();
+    // }));
+
+    // this.stores.patchValue(this.dataStores);
+  }
+
+  get getMyFormArray(): FormArray {
     return this.form.get('stores') as FormArray;
   }
 
-  ngOnInit(): void {
-    console.log(this.listSalesChannel);
-
-    this.buildForm();
-    this.loadValuesForm();
+  add() {
+    this.getMyFormArray.push(this.getModalityForGroup());
   }
 
-  buildForm(): void {
-    this.form = this.fb.group({
-      stores: this.fb.array<Store>([]),
-    });
-  }
-
-  /**
-   * Simulando o JSON de retorno da minha api, peguei e mockei o JSON da mesma forma que vem do back-end
-   */
-  loadValuesForm(): void {
-    this.dataStores = [
-      {
-        storeId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-        storeName: 'NOME',
-        storeType: {
-          id: 0,
-          name: 'string',
-          nickName: 'string',
-          active: true,
-        },
-        brand: {
-          id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-          name: 'string',
-          image: 'string',
-          active: true,
-        },
-        storeAddress: {
-          id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-          storeId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-          street: 'string',
-          number: 'string',
-          zipCode: 'string',
-          additional: 'string',
-          neighborhood: 'string',
-          city: 'string',
-          state: 'string',
-          country: 'string',
-          latitude: 0,
-          longitude: 0,
-          urlMaps: 'string',
-        },
-        contacts: [
-          {
-            id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-            name: 'string',
-            email: 'string',
-            phone: 0,
-            contactTypeId: 0,
-            contactTypeName: 'string',
-          },
-        ],
-        storeModalitys: [
-          {
-            storeModalityId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-            storeCode: 'code store 1',
-            cashierCode: 'code store 1',
-            modality: {
-              id: '3fa85f64-5717-4562-b3fc-2c963f66afAA',
-              name: 'string',
-              tag: 'string',
-              active: true,
-              order: 0,
-              canBlock: true,
-            },
-            openingHours: [
-              {
-                id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-                storeModalityId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-                dayWeek: 0,
-                initialTime: 'string',
-                endTime: 'string',
-              },
-            ],
-            salesChannel: [
-              {
-                storeModalitySalesChannelId:
-                  '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-                salesChannelId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-                salesChannelName: 'string',
-              },
-            ],
-          },
-        ],
-      },
-      {
-        storeId: '3fa85f64-5717-4562-b3fc-2c963f661111',
-        storeName: 'NOME',
-        storeType: {
-          id: 0,
-          name: 'string',
-          nickName: 'string',
-          active: true,
-        },
-        brand: {
-          id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-          name: 'string',
-          image: 'string',
-          active: true,
-        },
-        storeAddress: {
-          id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-          storeId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-          street: 'string',
-          number: 'string',
-          zipCode: 'string',
-          additional: 'string',
-          neighborhood: 'string',
-          city: 'string',
-          state: 'string',
-          country: 'string',
-          latitude: 0,
-          longitude: 0,
-          urlMaps: 'string',
-        },
-        contacts: [
-          {
-            id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-            name: 'string',
-            email: 'string',
-            phone: 0,
-            contactTypeId: 0,
-            contactTypeName: 'string',
-          },
-        ],
-        storeModalitys: [
-          {
-            storeModalityId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-            storeCode: 'code store 2',
-            cashierCode: 'code store 2',
-            modality: {
-              id: '3fa85f64-5717-4562-b3fc-2c963f66afBB',
-              name: 'string',
-              tag: 'string',
-              active: true,
-              order: 0,
-              canBlock: true,
-            },
-            openingHours: [
-              {
-                id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-                storeModalityId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-                dayWeek: 0,
-                initialTime: 'string',
-                endTime: 'string',
-              },
-            ],
-            salesChannel: [
-              {
-                storeModalitySalesChannelId:
-                  '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-                salesChannelId: '3fa85f64-5717-4562-b3fc-2c963f66af33',
-                salesChannelName: 'string',
-              },
-            ],
-          },
-        ],
-      },
-    ];
-
-    const response = this.dataStores.map(res => {
-      return res.storeModalitys.map(item => {
-        return {
-          storeId: res.storeId,
-          storeName: res.storeName,
-          storeCode: item.storeCode,
-          cashierCode: item.cashierCode,
-          modalityId: item.modality.id,
-          salesChannelId: item.salesChannel.map(item => item.salesChannelId),
-        };
-      });
-    });
-
-    response.flat().forEach(element => {
-      this.stores.push(this.fb.group(element));
-    });
+  getModalityForGroup(): FormGroup {
+    return this.formBuilder.group({
+      id: this.formBuilder.control(this.getMyFormArray.controls.length + 1),
+      storeModality: this.formBuilder.group({
+        storeId: this.formBuilder.control('', Validators.compose([Validators.required])),
+        modalityId: this.formBuilder.control('', Validators.compose([Validators.required])),
+        storeCode: this.formBuilder.control(''),
+        cashierCode: this.formBuilder.control('')
+      }),
+      openingHours: this.formBuilder.array([
+        this.formBuilder.group({
+          dayWeek: this.formBuilder.control(''),
+          initialTime: this.formBuilder.control(''),
+          endTime: this.formBuilder.control(''),
+        })
+      ]),
+      salesChannel: this.formBuilder.array([
+        this.formBuilder.group({
+          salesChannelId: this.formBuilder.control(''),
+          token: this.formBuilder.control(''),
+        })
+      ]),
+    })
   }
 
   remove(index: number): void {
-    this.stores.removeAt(index);
-  }
-
-  add(): void {
-    this.stores.push(
-      this.fb.group({
-        storeId: this.storeId, // pegarei o storeId da rota
-        modalityId: [null, Validators.required],
-        storeCode: [''],
-        cashierCode: [''],
-        salesChannelId: [null],
-      })
-    );
+    this.getMyFormArray.removeAt(index);
   }
 }
